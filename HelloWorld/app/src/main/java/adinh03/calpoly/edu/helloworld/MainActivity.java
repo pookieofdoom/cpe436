35ecbce9b48849d4e0eae77c53a01fe4c4ad7f23
Anthony Dinh <pookieofdoom@gmail.com>
2016-09-28 13:02:13 -0700

    hello world 90% done

--- HelloWorld/app/src/main/java/adinh03/calpoly/edu/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/adinh03/calpoly/edu/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package adinh03.calpoly.edu.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ImageView iv = (ImageView) findViewById(R.id.image1);
+        //Picasso.with(this).load(R.drawable.anthonydinhimage).into(iv);
     }
 }
