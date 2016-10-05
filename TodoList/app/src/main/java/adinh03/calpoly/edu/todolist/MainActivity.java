6e1daf2da8a224da18bd58409dca03489b0e9b21
Anthony Dinh <pookieofdoom@gmail.com>
2016-10-05 01:36:28 -0700

    hell yeah

--- TodoList/app/src/main/java/adinh03/calpoly/edu/todolist/MainActivity.java
+++ TodoList/app/src/main/java/adinh03/calpoly/edu/todolist/MainActivity.java
@@ -15,34 +15,35 @@ import android.widget.Toast;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+   private LinearLayout list;
+   private Button submitButton;
+   private EditText editText;
+   private LayoutInflater inflater;
+   private TextView addText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final EditText editText = (EditText) findViewById(R.id.entryText);
-        Button submitButton = (Button) findViewById(R.id.submitButton);
-        String entry;
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String entry = editText.getText().toString();
-                if (!entry.isEmpty())
-                {
-                   Log.d("DEBUG", entry);
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+       editText = (EditText) findViewById(R.id.entryText);
+       submitButton = (Button) findViewById(R.id.submitButton);
+       inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+       String entry;
+       list = (LinearLayout) findViewById(R.id.list);
+       submitButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+             String entry = editText.getText().toString();
+             editText.setText("");
+             if (!entry.isEmpty()) {
+                LinearLayout temp = (LinearLayout) inflater.inflate(R.layout.todoentry, null);
+                addText = (TextView) temp.findViewById(R.id.newText);
+                addText.setText(entry);
+                list.addView(temp);
+             }
+          }
 
-                   Log.d("DEBUG", "I made it here");
-
-                }
-            }
-        });
-
-       LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-       LinearLayout list = (LinearLayout) findViewById(R.id.list);
-       TextView temp = (TextView)inflater.inflate(R.layout.todoentry, null);
-       //list.addView(temp);
-       list.addView(temp, LinearLayout.LayoutParams.MATCH_PARENT);
-       Log.d("DEBUG", list.getChildAt(0).toString());
+       });
     }
+
 }
