9cabab97cc47ae64f8b280f5f258901656cba852
Anthony Dinh <pookieofdoom@gmail.com>
2016-10-05 01:01:31 -0700

    nothing working :0

--- TodoList/app/src/main/java/adinh03/calpoly/edu/todolist/MainActivity.java
+++ TodoList/app/src/main/java/adinh03/calpoly/edu/todolist/MainActivity.java
@@ -1,10 +1,16 @@
 package adinh03.calpoly.edu.todolist;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
 
@@ -23,10 +29,20 @@ public class MainActivity extends AppCompatActivity {
                 String entry = editText.getText().toString();
                 if (!entry.isEmpty())
                 {
-                    Log.d("DEBUG", entry);
+                   Log.d("DEBUG", entry);
+
+                   Log.d("DEBUG", "I made it here");
+
                 }
             }
         });
 
+       LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+       LinearLayout list = (LinearLayout) findViewById(R.id.list);
+       TextView temp = (TextView)inflater.inflate(R.layout.todoentry, null);
+       //list.addView(temp);
+       list.addView(temp, LinearLayout.LayoutParams.MATCH_PARENT);
+       Log.d("DEBUG", list.getChildAt(0).toString());
     }
 }
